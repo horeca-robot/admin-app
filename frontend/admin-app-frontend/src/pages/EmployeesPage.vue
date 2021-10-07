dbd15706c7dc4a6560fcc22d21e53c0d178cffcd
Onrust,Bas B <b.onrust@student.fontys.nl>
2021-10-07 09:38:48 +0200

    Add Employees front-end: username+pincode required

--- frontend/admin-app-frontend/src/pages/EmployeesPage.vue
+++ frontend/admin-app-frontend/src/pages/EmployeesPage.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Employees</h1>
+    <div id="employees">
+        <h1>Employees</h1>
+        <input type="text" placeholder="add username here" v-model="employeeUsername" @keyup.enter="addEmployee">
+        <input type="text" placeholder="add pincode here" v-model="employeePincode" @keyup.enter="addEmployee">
+        <ul>
+            <li v-for="employee of employees" :key="employee.id" >{{employee.username}} , {{employee.pincode}}</li>  
+        </ul>
+    </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "employees",
+  data() {
+      return {
+          employees: [],
+          employeeUsername: '',
+          employeePincode: ''
+      };
+  },
+  async created() {
+      try {
+          const res = await axios.get("http://localhost:3000/employees");
+
+          this.employees = res.data;
+      } catch (e) {
+          console.error(e);
+      }
+  },
+  methods: {
+      async addEmployee() {
+          if (this.employeeUsername != '' && this.employeePincode != '') {
+            const res = await axios.post("http://localhost:3000/employees", { username: this.employeeUsername, pincode: this.employeePincode});
+
+            this.employees = [...this.employees, res.data];
+
+            this.employeeUsername = '';
+            this.employeePincode = '';
+          }
+      }
+  }
+}
+</script>
+
+<style scoped>
+#employees input:first-of-type {
+    margin-right: 10px;
+}
+</style>
