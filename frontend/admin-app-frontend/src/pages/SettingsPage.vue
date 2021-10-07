473b537c6178a8381e64f8080af7fa6f1e074a77
Boris <Borismulder@outlook.com>
2021-10-07 20:16:38 +0200

    Image preview
    Made a begin on being able to select images, going to move the upload part to a component, added image preview so you can see the image before uploading.

--- frontend/admin-app-frontend/src/pages/SettingsPage.vue
+++ frontend/admin-app-frontend/src/pages/SettingsPage.vue
@@ -1,3 +1,60 @@
 <template>
-    <h1>Settings</h1>
+  <div id="container">
+    <div>
+        <label for="name">Restaurant Name&nbsp;</label>
+        <input id="name" ref="name" type="text" v-model="restaurantName">
+        {{ restaurantName }}
+    </div>
+    <br>
+    <br>
+    <div id="imagePreviewWrapper">
+        <label for="name">Restaurant Logo</label>
+      <img style="width: 100% !important; height: 100% !important" :src="`${previewImageData}`"/>
+    </div>
+
+    <input ref="fileInput" accept="image/png, image/gif, image/jpeg" type="file" @input="previewImage" />
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      restaurantName: "",
+      previewImageData: null,
+    };
+  },
+
+  methods: {
+    previewImage() {
+      let input = this.$refs.fileInput;
+
+      let file = input.files;
+
+      if (file && file[0]) {
+        let reader = new FileReader();
+
+        reader.onload = (e) => {
+          this.previewImageData = e.target.result;
+        };
+
+        reader.readAsDataURL(file[0]);
+
+        this.$emit("input", file[0]);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#imagePreviewWrapper {
+  width: 50%;
+  height: 50%;
+}
+
+#container{
+    margin-top: 2.5%;
+    margin-left: 2.5%;
+}
+</style>
