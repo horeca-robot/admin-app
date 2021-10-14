6f7d66cd0e7ed34bc5e8156cdf78c51aabb42053
ruben <462404@student.fontys.nl>
2021-10-14 13:40:59 +0200

    Code Cleaning

--- frontend/admin-app-frontend/src/App.vue
+++ frontend/admin-app-frontend/src/App.vue
@@ -24,21 +24,6 @@ export default {
       }
       return false;
     }
-  },
-  methods: {
-    // getTextColor(){
-    //   const style = getComputedStyle(document.body)
-    //   const secondary = style.getPropertyValue('--secondary-color')
-
-    //   var rgb = secondary.match(/\d+/g); //Get rgb values from --secondary-color variable
-
-    //   if((rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114) > 186){
-    //     document.documentElement.style.setProperty('--text-color', 'rgba(0, 0, 0, 1)');
-    //   }
-    //   else{
-    //     document.documentElement.style.setProperty('--text-color', 'rgba(255, 255, 255, 1)');
-    //   }
-    // }
   }
 }
 </script>
