37267c784896f82ce4858f36c3343522b5ae8ad6
ruben <462404@student.fontys.nl>
2021-10-14 13:49:57 +0200

    Update ColorUtil.js

--- frontend/admin-app-frontend/src/utils/ColorUtil.js
+++ frontend/admin-app-frontend/src/utils/ColorUtil.js
@@ -1,15 +1,20 @@
 export default {
     getTextColor(){
-        const style = getComputedStyle(document.body)
-        const secondary = style.getPropertyValue('--secondary-color')
+        const style = getComputedStyle(document.body);
+        const secondary = style.getPropertyValue('--secondary-color');
 
-        var rgb = secondary.match(/\d+/g); //Get rgb values from --secondary-color variable
-
-        if((rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114) > 186){
-            document.documentElement.style.setProperty('--text-color', 'rgba(0, 0, 0, 1)');
+        //Get rgb values from secondary variable
+        var rgb = secondary.match(/\d+/g);
+        
+        let textColor;
+        //https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+        if((rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114) > 186){ 
+            textColor = 'rgba(0, 0, 0, 1)';
         }
         else{
-            document.documentElement.style.setProperty('--text-color', 'rgba(255, 255, 255, 1)');
+            textColor = 'rgba(255, 255, 255, 1)';
         }
+
+        document.documentElement.style.setProperty('--text-color', textColor);
     }
 }
