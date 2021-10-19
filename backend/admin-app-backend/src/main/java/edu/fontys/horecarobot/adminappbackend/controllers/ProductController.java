934369feb717b1d8b09bb90c8bd6dffaae36d7e6
Max <m.neijndorff1999@gmail.com>
2021-10-19 12:05:41 +0200

    Feedback pull request

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/controllers/ProductController.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package edu.fontys.horecarobot.adminappbackend.controllers;
 
 import edu.fontys.horecarobot.adminappbackend.dtos.ApiResponse;
+import edu.fontys.horecarobot.adminappbackend.dtos.ProductModel;
 import edu.fontys.horecarobot.adminappbackend.services.ProductService;
 import edu.fontys.horecarobot.databaselibrary.models.Product;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +17,14 @@ public class ProductController {
 
     private final ProductService productService;
 
-
     @PostMapping
-    public ResponseEntity<ApiResponse> CreateProduct(@RequestBody Product product)
+    public ResponseEntity<?> createProduct(@RequestBody ProductModel product)
     {
-        if(product.getName().isBlank() || product.getPrice() <= 0 || product.getDiscountPrice() <= 0)
+        if(product.getName().isBlank() || product.getPrice() <= 0)
             return new ResponseEntity<>(ApiResponse.error("Not all fields are filled in"), HttpStatus.BAD_REQUEST);
 
-        if(productService.PostProduct(product))
-            return new ResponseEntity<>(ApiResponse.ok("Product has been posted to the database"), HttpStatus.OK);
-        else
-            return new ResponseEntity<>(ApiResponse.error(), HttpStatus.BAD_REQUEST);
+        productService.saveProduct((product));
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
 
 }
--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/services/ProductService.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/services/ProductService.java
@@ -1,5 +1,6 @@
 package edu.fontys.horecarobot.adminappbackend.services;
 
+import edu.fontys.horecarobot.adminappbackend.dtos.ProductModel;
 import edu.fontys.horecarobot.databaselibrary.models.Product;
 import edu.fontys.horecarobot.databaselibrary.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,18 +14,27 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-
     public List<Product> getAllProducts(){
         return productRepository.findAll();
     }
 
-
-    public boolean PostProduct( Product product)
+    public void saveProduct(ProductModel productModel)
     {
-        var result = productRepository.save((product));
-        if(result != null)
-            return true;
-        else
-            return false;
+        var product = createProduct(productModel);
+        productRepository.save(product);
     }
+
+    private Product createProduct(ProductModel productModel)
+    {
+        Product p = new Product();
+        p.setName(productModel.getName());
+        p.setDescription(productModel.getDescription());
+        p.setPrice(productModel.getPrice());
+        p.setContainsAlcohol(productModel.isContainsAlcohol());
+        p.setDiscountPrice(productModel.getDiscountPrice());
+        p.setImage(productModel.getImage());
+
+        return p;
+    }
+
 }
