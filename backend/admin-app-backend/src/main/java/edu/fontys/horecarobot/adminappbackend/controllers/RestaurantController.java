10abecb9f492a69e33202440c1c5ad72ec57fafe
Boris <Borismulder@outlook.com>
2021-10-20 17:35:06 +0200

    Controller + post model for the restaurant

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/controllers/RestaurantController.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/controllers/RestaurantController.java
@@ -1,24 +1,22 @@
 package edu.fontys.horecarobot.adminappbackend.controllers;
 
+import edu.fontys.horecarobot.adminappbackend.dtos.ApiResponse;
+import edu.fontys.horecarobot.adminappbackend.dtos.RestaurantModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "*")
 @RestController
+@CrossOrigin(origins = "http://localhost:4000")
 @RequestMapping("api/restaurant")
 public class RestaurantController {
 
-    @PostMapping("/upload/logo")
-    public String UploadLogo(@RequestParam("image") MultipartFile multipartFile)
+    @PostMapping(path = "/save/settings")
+    public ResponseEntity<ApiResponse> saveSettings(@RequestBody RestaurantModel model)
     {
-        if(multipartFile.isEmpty()) return "File is empty";
-
-        return String.format("File %s has been uploaded", multipartFile.getName());
+        // TODO check if some of the data is null or unchanged with saved version
+        // TODO save to database with db library
+        return ResponseEntity.ok(ApiResponse.ok("Settings have been saved!").addData("name",model.getName()));
     }
 
-    @GetMapping(path = "/test")
-    public String PleaseWork()
-    {
-        return  "I Work";
-    }
 }
---
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/dtos/RestaurantModel.java
@@ -0,0 +1,18 @@
+package edu.fontys.horecarobot.adminappbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class RestaurantModel {
+
+    private String name;
+
+    private String primarycolor;
+
+    private String secondarycolor;
+
+    private String logo;
+
+    private String background;
+
+}
