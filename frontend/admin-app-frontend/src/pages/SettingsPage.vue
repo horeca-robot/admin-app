46ae6d982b550739cff87b73fb7094772bae22cf
Boris <Borismulder@outlook.com>
2021-10-20 17:36:09 +0200

    Figma design for settings page + posting settings to backend
    Still needs some more fields, but need to add them to the design first

--- frontend/admin-app-frontend/src/pages/SettingsPage.vue
+++ frontend/admin-app-frontend/src/pages/SettingsPage.vue
@@ -3,38 +3,55 @@
     <h2>Settings</h2>
     <hr id="divider" />
     <br />
-    <div id="inputs">
-      <label id="nameLabel" for="name">Restaurant Name&nbsp;</label>
-      <input id="name" type="text" v-model="restaurantName" />
 
-      <label id="primaryColorLabel" for="primaryColor">Primary Color Website&nbsp;</label>
-      <input id="primaryColor" type="color" v-model="primaryColor" />
+    <div id="container-body">
+      <div id="inputs">
+        <label id="nameLabel" for="name">Restaurant Name&nbsp;</label>
+        <input id="name" type="text" v-model="restaurantName" />
 
-      <label id="secondaryColorLabel" for="secondaryColor">Secondary Color Website&nbsp;</label>
-      <input id="secondaryColor" type="color" v-model="secondaryColor" />
-    </div>
+        <label id="primaryColorLabel" for="primaryColor"
+          >Primary Color Website&nbsp;</label
+        >
+        <input id="primaryColor" type="color" v-model="primaryColor" />
 
-    <div class="row">
-      <div class="block">
-        <label for="imagePreview">Website Logo</label>
-        <ImagePreview id="imagePreview" ref="preview" />
+        <label id="secondaryColorLabel" for="secondaryColor"
+          >Secondary Color Website&nbsp;</label
+        >
+        <input id="secondaryColor" type="color" v-model="secondaryColor" />
       </div>
 
-      <div class="block">
-        <label for="imagePreview2">Background Website</label>
-        <ImagePreview id="imagePreview2" ref="preview2" />
+      <div class="row">
+        <div class="block">
+          <label for="imagePreview">Website Logo</label>
+          <ImagePreview class="m-t" id="imagePreview" ref="websiteLogo" />
+        </div>
+
+        <div class="block" style="margin-left: 8% !important">
+          <label for="imagePreview2">Background Website</label>
+          <ImagePreview
+            class="m-t"
+            id="imagePreview2"
+            ref="websiteBackground"
+          />
+        </div>
+
+        <div class="block">&nbsp;</div>
       </div>
-      <div class="block"></div>
     </div>
 
-    <button v-on:click="uploadImage">Test</button>
+    <hr id="divider" />
+
+    <div id="container-footer">
+      <button type="submit" id="save-btn" v-on:click="saveSettings">
+        Save
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import ImagePreview from "../components/ImagePreview.vue";
-import api from "../wrappers/Test.js"
-
+import api from "../wrappers/RestaurantWrapper"
 
 export default {
   data() {
@@ -50,9 +67,23 @@ export default {
   },
 
   methods: {
-   async uploadImage() {
-      //console.log(this.$refs.preview.base64);
-      await api.getTest();
+    async saveSettings() {
+      
+      var logo = this.$refs.websiteLogo.base64;
+
+      var background = this.$refs.websiteBackground.base64;
+
+      const payload = {
+        name: this.restaurantName,
+        primarycolor: this.primaryColor,
+        secondarycolor: this.secondaryColor,
+        logo: logo,
+        background: background,
+      }
+
+      var response = await api.saveSettings(payload);
+
+      console.log(response)
     },
   },
 };
@@ -61,10 +92,41 @@ export default {
 <style scoped>
 input {
   height: 30px;
+  font-size: 16px;
 }
 
-body {
-  box-sizing: border-box;
+#container-footer {
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  padding-bottom: 2.5%;
+}
+
+button {
+  display: flex;
+  height: 40px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #2d6ace;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  font-size: 18px;
+  transition-duration: 0.3s;
+}
+
+button:hover {
+  background-color: #0157e4;
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 5px 5px 0 rgba(0, 0, 0, 0.19);
+}
+
+button:active {
+  transform: translateY(4px);
+}
+
+#container-body {
+  overflow: hidden scroll;
+  height: 36rem;
 }
 
 #imagePreviewWrapper {
@@ -73,7 +135,7 @@ body {
 }
 
 #container {
-  margin-top: 5%;
+  margin-top: 2.5%;
   margin-left: 4%;
   margin-right: 2.5%;
   width: 80%;
@@ -120,5 +182,9 @@ label {
 }
 .block {
   width: 45%;
+}
+
+.m-t {
+  margin-top: 10px !important;
 }
 </style>
