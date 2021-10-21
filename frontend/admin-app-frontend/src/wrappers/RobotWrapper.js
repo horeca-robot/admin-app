67e52b3ba57ec3e9d7d14806fe15ce6eecfb454e
ruben <462404@student.fontys.nl>
2021-10-21 12:40:42 +0200

    Update Robot Frontend

--- frontend/admin-app-frontend/src/wrappers/RobotWrapper.js
+++ frontend/admin-app-frontend/src/wrappers/RobotWrapper.js
@@ -3,16 +3,19 @@ import axios from "axios";
 // Base url can be found in the .env file in the root folder
 export const baseUrl = process.env.VUE_APP_API_BASE_URL;
 
+var apiConfig = {
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+    }
+}
+
 export default {
+
     async getRobots(){
         var response;
 
         try{
-            response =  await axios.get(`${baseUrl}/api/Robot`, {
-                headers: {
-                    Authorization: 'Bearer ' + localStorage.getItem('jwt')
-                }
-            })
+            response =  await axios.get(`${baseUrl}/api/Robot`, apiConfig)
         }
         catch(error){
             response = error.response
@@ -27,37 +30,52 @@ export default {
 
     async postRobot(data) {
 
-        let config = {
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('jwt'),
-            }
-          }
-
         var response;
 
         try{
-            response =  await axios.post(`${baseUrl}/api/Robot`, data, config)
-            //response = await axios.get(`${baseUrl}/api/Robot/test`, config)
+            response =  await axios.post(`${baseUrl}/api/Robot`, data, apiConfig)
+
+            return {
+                success: response.status === 201
+            }
         }
         catch(error){
             response = error.response
-        }
 
-        return {
-            status: response.status
+            return {
+                success: response.data["success"],
+                message: response.data["message"]
+            }
         }
     },
 
-    async deleteRobot(data) {
+    async putRobot(data) {
 
         var response;
 
         try{
-            response =  await axios.delete(`${baseUrl}/api/Robot/${data}`, {
-                header: {
-                    Authorization: 'Bearer ' + localStorage.getItem('jwt')
-                }
-            })
+            response =  await axios.put(`${baseUrl}/api/Robot`, data, apiConfig)
+
+            return {
+                success: response.status === 201
+            }
+        }
+        catch(error){
+            response = error.response
+
+            return {
+                success: response.data["success"],
+                message: response.data["message"]
+            }
+        }
+    },
+
+    async deleteRobot(id) {
+
+        var response;
+
+        try{
+            response =  await axios.delete(`${baseUrl}/api/Robot/${id}`, apiConfig)
         }
         catch(error){
             response = error.response
@@ -65,8 +83,7 @@ export default {
 
         return {
             success: response.data["success"],
-            message: response.data["message"],
-            data: response.data["data"]
+            message: response.data["message"]
         }
     }
 } 
