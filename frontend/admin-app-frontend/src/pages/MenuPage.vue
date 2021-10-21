816b86c43c37d85f9ba698701937ea5c7b71c928
Mneijndorff <33960298+Mneijndorff99@users.noreply.github.com>
2021-10-21 14:41:15 +0100

    Update MenuPage.vue

--- frontend/admin-app-frontend/src/pages/MenuPage.vue
+++ frontend/admin-app-frontend/src/pages/MenuPage.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <AddProduct @add-product="AddProduct"/>
-        <button @click="GetProducts">Get</button>
+        <AddProduct @add-product="addProduct" class="panel"/>
+        <button @click="getProducts">Get</button>
     </div>
 </template>
 
 <script>
 import AddProduct from '../components/menuComponents/AddProduct.vue'
-import axios from 'axios'
+import ProductWrapper from '../wrappers/ProductWrapper.js'
 
 export default {
     name: 'MenuPage',
@@ -20,33 +20,21 @@ export default {
         }
     },
     methods: {
-        async AddProduct(product){
-            // this.products = [...this.products, product]
-            console.log(product)
-            // TODO Change connection string here 
-            const response = await axios.post("http://localhost:5000/products", product)
-                    .then(response =>{this.products = response.data})
-                    .catch(error => console.log(error.response))
-
+        async addProduct(product){
+            const response = await ProductWrapper.postProduct(product)
             console.log(response)
         },
 
-        async GetProducts(){
-            // TODO Change connection string here 
-            await axios.get("http://localhost:5000/products")
-                .then(response => {this.products = response.data, console.log(this.products.length)})
-                .catch(error => console.log(error))
-
+        async getProducts(){
+            const response = await ProductWrapper.getProducts()
+            console.log(response)
         }
     },
-
-    created(){
-        this.products = this.GetProducts()
-        console.log("Hello")
-        console.log(this.products.length)
-        for(var i =0; i < this.products.length; i++){
-            console.log(this.products.product.name)
-        }
-    }
 }
 </script>
+
+<style scoped>
+    .panel{
+        background-color: pink;
+    }
+</style>
