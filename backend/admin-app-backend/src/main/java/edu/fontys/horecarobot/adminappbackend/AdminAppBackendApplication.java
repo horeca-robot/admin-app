9fc995b3f66e44691d11ff94a8f6e9067772ecc2
ruben <462404@student.fontys.nl>
2021-10-21 12:42:59 +0200

    CodeCleaning

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/AdminAppBackendApplication.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/AdminAppBackendApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("edu.fontys.horecarobot.databaselibrary")
@@ -20,4 +17,5 @@ public class AdminAppBackendApplication {
 			ex.printStackTrace();
 		}
 	}
+	
 }
