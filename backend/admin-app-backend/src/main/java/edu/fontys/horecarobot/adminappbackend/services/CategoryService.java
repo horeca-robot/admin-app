04b84d74ca56c4257adb1736fa2e8043fad2105b
ruben <462404@student.fontys.nl>
2021-11-09 19:26:27 +0100

    Update CategoryModel

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/services/CategoryService.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/services/CategoryService.java
@@ -1,16 +1,16 @@
 package edu.fontys.horecarobot.adminappbackend.services;
 
 import edu.fontys.horecarobot.adminappbackend.dtos.CategoryModel;
-import edu.fontys.horecarobot.adminappbackend.dtos.ProductModel;
 import edu.fontys.horecarobot.databaselibrary.models.Category;
-import edu.fontys.horecarobot.databaselibrary.models.Product;
 import edu.fontys.horecarobot.databaselibrary.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,13 +18,20 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
-    }
-
     public boolean doesCategoryExist(UUID id)
     {
         return categoryRepository.findById(id).isPresent();
+    }
+
+    public List<CategoryModel> getAllCategories() {
+        var list = new ArrayList<CategoryModel>();
+        var categories = categoryRepository.findAll();
+
+        for(var category:categories){
+            list.add(convertToCategoryModel(category));
+        }
+
+        return list;
     }
 
     public Optional<Category> getById(UUID id)
@@ -32,16 +39,14 @@ public class CategoryService {
         return categoryRepository.findById(id);
     }
 
-    public void addCategory(CategoryModel categoryModel)
-    {
+    public void addCategory(CategoryModel categoryModel) {
         categoryRepository.saveAndFlush(convertFromCategoryModel(categoryModel));
     }
 
-    public Category updateCategory(CategoryModel categoryModel, UUID id) {
+    public void updateCategory(CategoryModel categoryModel, UUID id) {
         Category c = convertFromCategoryModel(categoryModel);
         c.setId(id);
         categoryRepository.saveAndFlush(c);
-        return c;
     }
 
     public void deleteCategory(UUID id)
@@ -49,11 +54,36 @@ public class CategoryService {
         categoryRepository.deleteById(id);
     }
 
+    public CategoryModel convertToCategoryModel(Category category){
+        CategoryModel c = new CategoryModel();
+        c.setId(category.getId());
+        c.setName(category.getName());
+        c.setImage(category.getImage());
+        c.setParentCategories(category.getParentCategories().stream().map(Category::getId).collect(Collectors.toList()));
+        var list = new ArrayList<CategoryModel>();
+        for(var child:category.getChildCategories()){
+            list.add(convertToCategoryModel(child));
+        }
+        c.setChildCategories(list);
+        return c;
+    }
+
     private Category convertFromCategoryModel(CategoryModel categoryModel){
         Category c = new Category();
-        c.setParentCategories(categoryModel.getParentCategory());
         c.setName(categoryModel.getName());
         c.setImage(categoryModel.getImage());
+        c.setParentCategories(getCategories(categoryModel.getParentCategories()));
         return c;
+    }
+
+    private List<Category> getCategories(List<UUID> categories){
+        var list = new ArrayList<Category>();
+
+        for (var id:categories) {
+            var category = getById(id);
+            category.ifPresent(list::add);
+        }
+
+        return list;
     }
 }
