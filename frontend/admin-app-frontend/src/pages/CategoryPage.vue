27a737772ae0ee15647e08d1a310784a576a7d5f
ruben <462404@student.fontys.nl>
2021-11-09 19:38:02 +0100

    Category Page Toegevoegd

---
+++ frontend/admin-app-frontend/src/pages/CategoryPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Category</h1>
+</template>
+
+<script>
+import CategoryWrapper from '../wrappers/CategoryWrapper'
+
+export default {
+    data(){
+        return{
+            id: '',
+            isEditing: false
+        }
+    },
+    created(){
+        this.id = this.$route.query.id
+
+        if(this.id !== undefined){
+            this.isEditing = true
+            this.getCategoryInfo()
+        }
+    },
+    methods: {
+        async getCategoryInfo(){
+            const response = await CategoryWrapper.getCategoryById(this.id)
+            console.log(response.category)
+        }
+    }
+}
+</script>
