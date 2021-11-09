646f44c12c90f24a58b5c37ee41dfee8a4f0553b
pieterdebot <pieter.de.bot@gmail.com>
2021-11-09 12:40:17 +0100

    Changed /employees to /employee

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/controllers/EmployeeController.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/controllers/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(path = "api/employees")
+@RequestMapping(path = "api/employee")
 @CrossOrigin(origins = "http://localhost:4000")
 @RequiredArgsConstructor
 public class EmployeeController {
