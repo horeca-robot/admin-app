86ba35e067e9e1dd98f77ef06e93e3c22175c71b
Boris <Borismulder@outlook.com>
2021-11-10 22:16:02 +0100

    Update RestaurantService.java

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/services/RestaurantService.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/services/RestaurantService.java
@@ -73,9 +73,8 @@ public class RestaurantService {
 
             if(restaurantInfo.isEmpty())
             {
-                // Create
                 var model = new RestaurantInfo();
-                model.setName(" ");
+                model.setName("");
 
                 restaurantInfoRepository.saveAndFlush(model);
                 return getRestaurantInfo();
