1a686ccc9ffeaf273eb5a4e772723a00c213970d
pieterdebot <pieter.de.bot@gmail.com>
2021-11-16 13:24:19 +0100

    Updated API Wrappers

--- frontend/admin-app-frontend/src/wrappers/InfoWrapper.js
+++ frontend/admin-app-frontend/src/wrappers/InfoWrapper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base url can be found in the .env file in the root folder
-export const baseUrl = process.env.VUE_APP_API_BASE_URL;
+export const baseUrl = `${process.env.VUE_APP_API_BASE_URL}/api/info`;
 
 var apiConfig = {
     headers: {
@@ -14,7 +14,7 @@ export default {
         var response;
 
         try {
-            response = await axios.get(`${baseUrl}/api/info/restaurant`, apiConfig)
+            response = await axios.get(`${baseUrl}/restaurant`, apiConfig)
         } catch (error) {
             response = error.response
         }
@@ -25,7 +25,7 @@ export default {
         var response;
 
         try {
-            response = await axios.put(`${baseUrl}/api/info/restaurant`, model, apiConfig)
+            response = await axios.put(`${baseUrl}/restaurant`, model, apiConfig)
         } catch (error) {
             response = error.response
         }
@@ -36,7 +36,7 @@ export default {
         var response;
 
         try {
-            response = await axios.get(`${baseUrl}/api/info/website`, apiConfig)
+            response = await axios.get(`${baseUrl}/website`, apiConfig)
         } catch (error) {
             response = error.response
         }
@@ -47,7 +47,7 @@ export default {
         var response;
 
         try {
-            response = await axios.put(`${baseUrl}/api/info/website`, model, apiConfig)
+            response = await axios.put(`${baseUrl}/website`, model, apiConfig)
         } catch (error) {
             response = error.response
         }
