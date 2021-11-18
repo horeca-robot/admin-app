263087653a14e3ac85ffe220ed7e53465d0fa5cb
pieterdebot <pieter.de.bot@gmail.com>
2021-11-18 10:58:14 +0100

    Update TagRequestModel.java

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/dtos/request/TagRequestModel.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/dtos/request/TagRequestModel.java
@@ -1,9 +1,11 @@
 package edu.fontys.horecarobot.adminappbackend.dtos.request;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 @Data
+@NoArgsConstructor
 public class TagRequestModel {
     @NonNull
     private String name;
