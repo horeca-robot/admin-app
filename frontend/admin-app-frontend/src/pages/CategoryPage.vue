0c4e719b1a005d687bea24494f7a37856932b53c
ruben <462404@student.fontys.nl>
2021-11-25 16:15:04 +0100

    Fixes

--- frontend/admin-app-frontend/src/pages/CategoryPage.vue
+++ frontend/admin-app-frontend/src/pages/CategoryPage.vue
@@ -1,73 +1,69 @@
 <template>
-    <div class="page">
-      <form class="panel" @submit="onSubmit">
-        <div class="panel-side">
-          <div class="blocks">
-              <label class="text"> Name:</label>
-              <input class="inputs" placeholder="Category Name" v-model="catName" name="catName"/>
-            </div>
-            <div class="blocks">
-              <label class="text"> Image:</label>
-              <div class="imageCom">
-                <ImagePreview ref="image" />
-              </div>
-            </div>
-        </div>
-        <div class="panel-side">
-          <div class="blocks">
-            <label class="text"> Select Parent-Categories:</label>
-            <div class="categorieHolder"> 
-                <div class="categories" v-for="category in categories" :key="category.id"> 
-                    <input type="checkbox" v-model="category.selected"> <label>{{category.name}}</label>
-                </div>
-            </div>
-          </div>
-            <div class="blocks-row-buttons">
-              <input class="button formbuttonSave" type="submit" value="Save"/>
-              <button class="button formbuttonDelete" @click="deleteCategory">Delete</button>
-            </div>
-        </div>
-      </form>  
+  <form id="panel" @submit="onSubmit">
+    <div class="panel-side">
+      <div class="panel-section">
+        <label>Name:</label>
+        <input placeholder="Name..." v-model="name" type="text"/>
+      </div>
+      <div class="panel-section">
+        <label>Image:</label>
+        <ImagePreview ref="image" />
+      </div>
     </div>
+    <div class="panel-side">
+      <div class="panel-section">
+        <label>Select Parent-Categories:</label>
+        <div id="categories">
+          <div class="category" v-for="category in filteredCategories" :key="category.id"> 
+              <input type="checkbox" v-model="category.selected"> 
+              <label>{{ category.name }}</label>
+          </div>
+        </div>
+      </div>
+      <div id="buttons">
+        <input class="button" id="save" type="submit" value="Save">
+        <button class="button" id="delete" @click="deleteCategory">Delete</button>
+      </div>
+    </div>
+  </form>
 </template> 
 
 <script>
 import CategoryWrapper from '../wrappers/CategoryWrapper'
 import ImagePreview from '../components/ImagePreview.vue'
+import notification from '../utils/NotificationUtil'
 
 export default {
-    data()
-    {
-      return{
-          id: '',
-          isEditing: false,
-          catName: '',
-          categories: [],
-          childCategories: [],
+    data() {
+      return {
+        id: '',
+        isEditing: false,
+        name: '',
+        filteredCategories: [],
+        allCategories: []
       }
     },
-    created(){
+    components:{
+      ImagePreview
+    },
+    async created() {
       this.id = this.$route.query.id
 
-      this.getAllCategories();
+      await this.getAllCategories();
 
       if(this.id){
           this.isEditing = true
           this.getCategoryInfo()
       }
     },
-    components:{
-      ImagePreview
-    },
     methods: {
       async getAllCategories(){
         const response = await CategoryWrapper.getCategories()
-        this.categories = response.categories
-        this.categories = this.categories.filter(c => !c.parentCategories.length && this.id !== c.id)
-        console.log(this.categories)
-        this.categories.forEach(function (category) {
-                category.selected = false
-            })
+        this.allCategories = response.categories
+        this.filteredCategories = response.categories.filter(c => !c.parentCategories.length && this.id !== c.id)
+        this.filteredCategories.forEach(function (category) {
+          category.selected = false
+        })
       },
 
       async getCategoryInfo(){
@@ -77,242 +73,202 @@ export default {
 
       setCategoryValues(category){
         this.id = category.id
-        this.catName = category.name
+        this.name = category.name
 
-        this.categories.forEach(function (c) {
+        console.log(category.parentCategories)
+        console.log(this.filteredCategories)
 
-            if(category.parentCategories.some(i => i === c.id)){
-                c.selected = true
-            }
+        this.filteredCategories.forEach(function (c) {
+          if(category.parentCategories.some(i => i === c.id)){
+            c.selected = true
+          }
         })
 
-        this.categories = this.categories.filter(function(value){ 
-         return value.id !== category.id;
-         });
-
         if(category.image){
-            this.$refs.image.setBase64(category.image)
+          this.$refs.image.setBase64(category.image)
         }
-        },
+      },
 
-      onSubmit(e){
+      async onSubmit(e){
         e.preventDefault()
 
-        if(!this.catName){
-          alert('Not all required fields are filled in, please check again')
+        if(!this.name){
+          notification.showErrorNotification('Not all required fields are filled in, please check again')
+          return
+        }
+
+        if(this.filteredCategories.some(c => c.name.toLowerCase() === this.name.toLowerCase())){
+          notification.showErrorNotification('Already a category exist with the same name.')
           return
         }
 
         if(!this.isEditing){
-          this.addCategory()
+          await this.addCategory()
         }
         else{
-          this.updateCategory()
+          await this.updateCategory()
         }
 
         this.resetValues()
+        this.getAllCategories()
         this.$router.push('category')
-
       },
 
       async addCategory(){
-        const payload =
-            {
-              name: this.catName,
-              image : this.$refs.image.base64,
-              parentCategories: this.categories.filter(i => i.selected).map(i => i.id)
-            }
+        const payload = {
+          name: this.name,
+          image : this.$refs.image.base64,
+          parentCategories: this.filteredCategories.filter(i => i.selected).map(i => i.id)
+        }
 
         const response = await CategoryWrapper.postCategory(payload)
 
         if(response.success){
-          alert(`Successfully added ${payload.name} to the menu.`)
+          notification.showSuccessNotification(`Successfully added ${payload.name} to the menu.`)
         }
-        else
-        {
-          alert(response.message)
+        else {
+          notification.showErrorNotification(response.message)
         }
       },
 
       async updateCategory(){
-        const payload =
-            {
-              id: this.id,
-              name: this.catName,
-              image : this.$refs.image.base64,
-              parentCategories: this.categories.filter(i => i.selected).map(i => i.id)
-            }
+        const payload = {
+          id: this.id,
+          name: this.name,
+          image : this.$refs.image.base64,
+          parentCategories: this.filteredCategories.filter(i => i.selected).map(i => i.id)
+        }
 
         const response = await CategoryWrapper.putCategory(payload)
 
         if(response.success){
-          alert(`Successfully updated ${payload.name}.`)
+          notification.showSuccessNotification(`Successfully updated ${payload.name}.`)
         }
-        else
-        {
-          alert(response.message)
+        else {
+          notification.showErrorNotification(response.message)
         }
       },
 
       async deleteCategory(e){
-            e.preventDefault()
+        e.preventDefault()
 
-            if(!this.isEditing){
-                this.$router.push('menu')
-                return
-            }
+        if(!this.isEditing){
+          this.$router.push('menu')
+          return
+        }
 
-            if(confirm(`Are you sure you wan't to delete ${this.catName} from the menu?`)){
-                await CategoryWrapper.deleteCategory(this.id)
-                this.$router.push('menu')
-                alert(`Succesfully deleted ${this.catName}.`)
-                this.resetValues()
-            }
-        },
+        if(confirm(`Are you sure you wan't to delete ${this.name} from the menu?`)){
+          await CategoryWrapper.deleteCategory(this.id)
+          this.$router.push('menu')
+          notification.showSuccessNotification(`Successfully deleted ${this.name}.`)
+          this.resetValues()
+        }
+      },
       
       resetValues(){
-        this.catName = ''
-        this.categories.forEach(function (category) {
-                category.selected = false
-            })
+        this.id = ''
+        this.name = ''
+        this.filteredCategories.forEach(function (category) {
+          category.selected = false
+        })
       }
     }
 }
 </script>
 
 <style scoped>
-
-.page{
-  top:0;
+#panel{ 
+  top: 0;
   bottom: 0;
   right: 0;
   margin: auto;
-  height: fit-content;
-  width: fit-content;
-}
-
-.panel{
-        min-width: 500px;
-        width: 31vw;
-        min-height: fit-content;
-        background-color: var(--secondary-color);
-        box-shadow: 5px 5px 5px 1px rgba(0, 0, 0, 0.5);
-        border-radius: 10px;
-        display: flex;
-        justify-content: space-between;
+  min-width: 500px;
+  width: 30vw;
+  min-height: fit-content;
+  height: 30vh;
+  background-color: var(--secondary-color);
+  box-shadow: 5px 5px 5px 1px rgb(0 0 0 / 50%);
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 30px;
 }
 
 .panel-side{
-display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 30px;
-}
-
-.backpanel{
-  background-color: var(--secondary-color);
-  width: 35vw;
-  height: fit-content;
-  border-radius: 10px;
-  padding: 30px;
-  box-shadow: 5px 5px 5px 1px rgba(0, 0, 0, 0.5);
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: fit-content;
 }
 
-.container{
-  display: inline-block;
-  width: 100%;
-}
-
-.leftBlock{
-  float:left;
-  width: 45%;
-  min-height: 10px;
-}
-
-.text{
-  font-size: 1.5rem;
-  margin-bottom: 2px;
-}
-
-.blocks{
+.panel-section{
   display: flex;
   flex-direction: column;
 }
 
-.blocks-row-buttons{
-  margin-top:20px;
-  margin-left: -5%;
+label{
+  font-size: 1.5rem;
+  margin-bottom: 2px;
+}
+
+input[type=text] {  
+  font-family: inherit;
+  font-size: 1rem;
+  border: 2px solid var(--primary-color);
+  border-radius: 5px;
+  width: calc(100% - 14px);
+  height: 30px;
+  padding: 5px;
+  margin-bottom: 5%;
+}
+
+#categories{
+  padding: 10px;
+  height: 150px;
+  width: calc(100% - 24px);
+  overflow-x: hidden;
+  overflow-y: auto;
+  border: 2px solid var(--primary-color);
+  border-radius: 5px;
+}
+
+.category{
+  color: var(--text-color);
   display: flex;
-  flex-direction: row;
+  align-items: center;
+}
+
+input[type=checkbox]{
+  transform: scale(1.3)
+}
+
+  .category label{    
+    font-size: 1.3em !important;
+    margin-left: 5px;
+  }
+
+#buttons{  
+  display: flex;
+  justify-content: space-between;
 }
 
 .button{
-  width: 50px;
-  background-color: var(--primary-color);
+  width: 40%;
   border: 2px solid var(--primary-color);
   border-radius: 5px;
-  color: white;
-  margin-left: 1%;
-  font-size: 1em;
+  font-size: 1.5em;
   font-family: inherit;
-}
-
-.button:hover{
   cursor: pointer;
+  padding: 5px;
 }
 
-.formbuttonSave{
-  margin-left: 10%;
-  height: 3vh;
-  width: 5vw;
-  font-size: 1rem;
+#save{
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
 }
 
-input[type=checkbox] {
-  transform: scale(1.3);
-}
-
-.formbuttonDelete{
-  margin-left: 10%;
-  height: 3vh;
-  width: 5vw;
-  background-color: white;
+#delete{
+  background-color: var(--secondary-color);
   color: var(--primary-color);
-  font-size: 1rem;
-}
-
-.inputs{
-  font-family: inherit;
-  font-size: 1rem;
-  border: 2px solid var(--primary-color);
-  border-radius: 5px;
-  width: 220px;
-  height: 30px;
-}
-
-.imageCom{
-  border: 2px solid var(--primary-color);
-  border-radius: 5px;
-}
-
-.inputs:focus{
-  outline: none;
-}
-
-.categorieHolder{
-        padding: 10px;
-        height: 150px;
-        width: 200px;
-        overflow: auto;
-        border: 2px solid var(--primary-color);
-        border-radius: 5px;
-    }
-
-.categories{
-    float: left;
-    height: auto;
-    width: 100%;
-    color: var(--text-color);
 }
 </style>
