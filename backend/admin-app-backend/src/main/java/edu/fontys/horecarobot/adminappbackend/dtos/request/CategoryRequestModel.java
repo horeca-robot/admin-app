3095fdff41bea2fbeec151adbc495ad60831c0f6
Max <m.neijndorff1999@gmail.com>
2021-12-02 09:24:58 +0100

    Update CategoryRequestModel.java

--- backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/dtos/request/CategoryRequestModel.java
+++ backend/admin-app-backend/src/main/java/edu/fontys/horecarobot/adminappbackend/dtos/request/CategoryRequestModel.java
@@ -15,4 +15,5 @@ public class CategoryRequestModel {
     private String name;
     private String image;
     private List<UUID> parentCategories = new ArrayList<>();
+    private Boolean visible;
 }
