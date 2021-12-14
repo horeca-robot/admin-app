ca5ccdbac444de81330b7de3fdfa18bdc5cf7a7b
Max <m.neijndorff1999@gmail.com>
2021-12-14 15:52:19 +0100

    Create ProductControllerTest.java

---
+++ backend/admin-app-backend/src/test/java/edu/fontys/horecarobot/adminappbackend/controllers/ProductControllerTest.java
@@ -0,0 +1,91 @@
+package edu.fontys.horecarobot.adminappbackend.controllers;
+
+import edu.fontys.horecarobot.adminappbackend.AdminAppBackendApplication;
+import edu.fontys.horecarobot.adminappbackend.dtos.request.ProductRequestModel;
+import edu.fontys.horecarobot.adminappbackend.utilities.JwtUtil;
+import edu.fontys.horecarobot.databaselibrary.models.AdminUser;
+import edu.fontys.horecarobot.databaselibrary.repositories.AdminUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = AdminAppBackendApplication.class)
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private AdminUserRepository adminUserRepository;
+    @Autowired
+    private JwtUtil jwtUtil;
+    private String token;
+
+    @BeforeEach
+    public void setUp(){
+        createAccount();
+    }
+
+    @Test
+    public void get_all_Products() throws Exception{
+        mvc.perform(get("/api/product")
+                .header("authorization", "Bearer " + token)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    public void post_new_product() throws Exception{
+//        mvc.perform(post("/api/product"))
+//    }
+
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        AdminUser exampleAdmin = new AdminUser();
+        exampleAdmin.setEmail(userName);
+        Example<AdminUser> example = Example.of(exampleAdmin);
+
+        Optional<AdminUser> optionalAdmin = adminUserRepository.findOne(example);
+
+        if (optionalAdmin.isPresent()) {
+            AdminUser admin = optionalAdmin.get();
+            return new User(admin.getEmail(), admin.getPassword(), new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User with username (email) not found!");
+        }
+    }
+
+    public void createAccount(){
+        var user = new AdminUser();
+        user.setPassword("Test");
+        user.setEmail("test@gmail.com");
+        adminUserRepository.saveAndFlush(user);
+
+        final UserDetails userDetails = loadUserByUsername(user.getEmail());
+        token = jwtUtil.generateToken(userDetails);
+    }
+    public ProductRequestModel product(){
+        ProductRequestModel product = new ProductRequestModel();
+        product.setName("Bread");
+        product.setPrice(5);
+        return product;
+    }
+
+}
